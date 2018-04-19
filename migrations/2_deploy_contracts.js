d7e98d792c17b41550c140a4f9e9768ddc221938
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-19 20:52:55 +0200

    add Ownable and Olayer to migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-const RockpaperScissors = artifacts.require("./RockpaperScissors.sol");
+const RockPaperScissors = artifacts.require("./RockPaperScissors.sol");
+const Ownable = artifacts.require("./Ownable.sol");
+const Player = artifacts.require("./Player.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(RockpaperScissors);
+module.exports = function(deployer, network, accounts) {
+    deployer.deploy(Ownable);
+    deployer.deploy(Player, accounts[1]);
+    deployer.deploy(RockPaperScissors);
 };
