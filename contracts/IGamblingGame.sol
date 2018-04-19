697caa1b0b0e3b28899fc069638d93d4ec3244f7
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-19 20:48:55 +0200

    first implemetation

--- contracts/IGamblingGame.sol
+++ contracts/IGamblingGame.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.4.21;
 
-import "./IGame.sol";
-
-contract IGamblingGame is IGame {
-	function bet() public payable returns (bool) {}
-	function widthdrawal(uint256 amount) public returns (bool) {}
+contract IGamblingGame {
+	function createGame(uint256 _betAmount, uint256 _endGame, uint8 _initialStatus) public returns (bool);
+	function addPlayer(address _player) public returns (bool);
+	function bet() public payable returns (bool);
+	function widthdrawal(uint256 amount) public returns (bool);
+	event LogNewGame(uint256 _betAmount, uint256 _endGame);
+	event LogAddPlayer(address _player);
 	event LogBet(uint256 value);
 	event LogWidthdrawal(address player, uint256 amount);
 }
