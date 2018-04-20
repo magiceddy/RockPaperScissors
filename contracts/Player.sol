123b087c3e102d2ca2c443bfd36c50e9525ffe41
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-20 15:30:00 +0200

    add setBet

--- contracts/Player.sol
+++ contracts/Player.sol
@@ -6,17 +6,27 @@ contract Player is Ownable {
 
 	address public playerAddress;
 	bytes32 public hashBet;
+	uint8 public bet;
 	bool public hasBet;
 
 	function Player(address _player) public {
 		playerAddress = _player;
 	}
 
-	function setHashBet(bytes32 _bet) public onlyOwner returns (bool) {
+	function setHashBet(bytes32 _hashBet) public onlyOwner returns (bool) {
 		require(!hasBet);
 
 		hasBet = true;
-		hashBet = _bet;
+		hashBet = _hashBet;
+		return true;
+	}
+
+	function setBet(uint8 _bet) public onlyOwner returns (bool) {
+		require(_bet >= 0 && _bet <= 3);
+		require(bet == 0x0);
+		require(hashBet != 0x0);
+
+		bet = _bet;
 		return true;
 	}
 }
--- test/Player.js
+++ test/Player.js
@@ -2,14 +2,16 @@ const Promise = require('bluebird');
 const Player = artifacts.require("./Player.sol");
 const TestUtils = require('../testUtils.js');
 
-Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+Promise.promisifyAll(web3, { suffix: "Promise" });
 Promise.promisifyAll(web3.personal, { suffix: "Promise" });
 
 contract('Player', accounts => {
 
 	const owner = accounts[1];
 	const player = accounts[2];
-	const bet = web3.sha3(0);;
+	const hashBet = web3.sha3('3', 'secretKey');
+	const bet = 3;
+
 	let instance;
 
 	beforeEach(async() => {
@@ -46,13 +48,13 @@ contract('Player', accounts => {
 		describe('fail case', () => {
 
 			it('hould fail with value in transaction', async() => {
-				await TestUtils.noValue(instance.setHashBet(bet, { from: owner, value: 1 }));
+				await TestUtils.noValue(instance.setHashBet(hashBet, { from: owner, value: 1 }));
 			});
 
 			it('should fail with hash already setted', async() => {
-				await instance.setHashBet(bet, { from: owner });
+				await instance.setHashBet(hashBet, { from: owner });
 				try {
-					const txObject = await instance.setHashBet(bet, { from: owner });
+					const txObject = await instance.setHashBet(hashBet, { from: owner });
 					assert.isUndefined(txObject, 'set already setted hash');
 				} catch (err) {
 					assert.include(
@@ -65,7 +67,7 @@ contract('Player', accounts => {
 
 			it('should fail with no owner transaction', async() => {
 				try {
-					const txObject = await instance.setHashBet(bet, { from: player });
+					const txObject = await instance.setHashBet(hashBet, { from: player });
 					assert.isUndefined(txObject, 'set already setted hash');
 				} catch (err) {
 					assert.include(
@@ -93,9 +95,81 @@ contract('Player', accounts => {
 		describe('success case', () => {
 
 			it('should set hash', async() => {
-				const txObject = await instance.setHashBet(bet, { from: owner });
+				const txObject = await instance.setHashBet(hashBet, { from: owner });
 				const hash = await instance.hashBet();
-				assert.equal(hash, bet, 'wrong bet');
+				assert.equal(hash, hashBet, 'wrong bet');
+			});
+		});
+	});
+
+	describe('setBet', () => {
+
+		describe('fail case', () => {
+
+			it('should fail with value in transaction', async() => {
+				await TestUtils.noValue(instance.setBet(bet, { from: owner, value: 1}));
+			});
+
+			it('should fail without owner', async() => {
+				try {
+					const txObject = await instance.setBet(bet, { from: player });
+					assert.isUndefined(txObject, 'bet allowed by non owner');
+				} catch (err) {
+					assert.include(err.message, 'revert', 'no revert for non owner bet');
+				}
+			});
+
+			it('should fail with not in range bet', async() => {
+				try {
+					const txObject = await instance.setBet(4, { from: owner });
+					assert.isUndefined(txObject, 'bet not in range allowed');
+				} catch (err) {
+					assert.include(
+						err.message,
+						'revert',
+						'no revert for not in range bet'
+					);
+				}
+			});
+
+			it('should fail if bet exists', async() => {
+				await instance.setHashBet(hashBet, { from: owner });
+				await instance.setBet(bet, { from: owner });
+
+				try {
+					const txObject = await instance.setBet(bet, { from: owner });
+					assert.isUndefined(txObject, 'second bet allowed');
+				} catch (err) {
+					assert.include(
+						err.message,
+						'revert',
+						'no revert for second bet'
+					);
+				}
+			});
+
+			it('should fail if hashBet doesn\'t exists', async() => {
+				try {
+					const txObject = await instance.setBet(bet, { from: owner });
+					assert.isUndefined(txObject, 'bet allowed without hashBet');
+				} catch (err) {
+					assert.include(
+						err.message,
+						'revert',
+						'no revert if hashBet not set'
+					);
+				}
+			});
+		});
+
+		describe('success case', async() => {
+
+			it('should set bet', async() => {
+				await instance.setHashBet(hashBet, { from: owner });
+				await instance.setBet(bet, { from: owner });
+
+				const _bet = await instance.bet();
+				assert.equal(_bet, bet, 'wrong bet');
 			});
 		});
 	});
