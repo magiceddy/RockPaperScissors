a1f263ef8d4727c1468a6ae6f92333f9003fed45
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-21 00:26:30 +0200

    delete widthdrawal

--- contracts/IGamblingGame.sol
+++ contracts/IGamblingGame.sol
@@ -4,9 +4,7 @@ contract IGamblingGame {
 	function createGame(uint256 _betAmount, uint256 _endGame) public returns (bool);
 	function addPlayer(address _player) public returns (bool);
 	function bet(address _player, bytes32 _bet) public payable returns (bool);
-	function widthdrawal(uint256 _amount) public returns (bool);
 	event LogNewGame(uint256 _betAmount, uint256 _endGame);
 	event LogNewPlayer(address _player);
 	event LogBet(address _player, bytes32 _value);
-	event LogWidthdrawal(address _player, uint256 _amount);
 }
