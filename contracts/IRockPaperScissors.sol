e483f9edad2dbda0cc86b49793952eb1eb5b0a66
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-20 20:58:09 +0200

    add parameters to revealBet

--- contracts/IRockPaperScissors.sol
+++ contracts/IRockPaperScissors.sol
@@ -4,7 +4,7 @@ import "./IGamblingGame.sol";
 
 contract IRockPaperScissors is IGamblingGame {
 	function checkWinner() public returns (bool);
-	function revealBet() public returns (bool);
+	function revealBet(address _player, uint8 _bet, bytes32 _secretKey) public returns (bool);
 	event LogWinner(address player);
-	event LogRevealBet();
+	event LogRevealBet(address _player, uint8 _bet, uint8 _totalBet);
 }
